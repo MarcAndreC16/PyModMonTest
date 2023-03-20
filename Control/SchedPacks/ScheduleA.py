3cfb31c64d9a5628309376fdbcc302054ef96cdc
MarcAndreC16 <macochet2000@gmail.com>
2023-03-20 14:34:14 +0100

    schedule modif

--- Control/SchedPacks/ScheduleA.py
+++ Control/SchedPacks/ScheduleA.py
@@ -1,9 +1,9 @@
  #  SCHEDULE FOR MEASUREMENTS
     # 
     #  Action : 0: as_given  (pause of value Duration)
-    #        	1: charge/discharge to given maxSOC
-    #        	(2: charge (to defined maxSOC with given P)
-    #        	(3: discharge (to defined maxSOC with given P)
+    #        	(1: charge/discharge to given maxSOC)
+    #        	2: charge (to defined maxSOC with given P)
+    #        	3: discharge (to defined maxSOC with given P)
     #        	(4: standby)
     #        	(5: default)
     #  Duration : 0 = do not wait
@@ -17,13 +17,13 @@ def schedule_A():
 		
 		#--------Start of Battery-Test------------
 		#---Conditioning---		
-		2: { 'Action' :1, 'P' : 100, 'Q' :0.0, 'maxSOC' :100, 'minSOC':0.0, 'Duration' : 0.0 * 60 },	#Charge to SOCmax=100%	
+		2: { 'Action' :2, 'P' : 100, 'Q' :0.0, 'maxSOC' :1, 'minSOC':0.0, 'Duration' : 0.0 * 60 },	#Charge to SOCmax=100%	
 		3: { 'Action' :0, 'P' :   0.0, 'Q' :0.0, 'maxSOC' :100, 'minSOC':0.0, 'Duration' :10 }, # Pause for 10 minutes
 		
 		#---Discharging/Charging with 100% nom. Power	(1of3)---
 		4: { 'Action' :3, 'P' : 100.0, 'Q' :0, 'maxSOC' :  0, 'minSOC':0.0, 'Duration' : 0.0 * 60 },	#Discharging
-		5: { 'Action' :0, 'P' :   0.0, 'Q' :0.0, 'maxSOC' :100, 'minSOC':0.0, 'Duration' : 5  }, # Pause for 5 minutes (not mandatory)
-		6: { 'Action' :2, 'P' : 100.0, 'Q' :0.0, 'maxSOC' :100, 'minSOC':0.0, 'Duration' : 0.0 * 60 }, #Charging
+		5: { 'Action' :0, 'P' :   0.0, 'Q' :0.0, 'maxSOC' :0.0, 'minSOC':0.0, 'Duration' : 5  }, # Pause for 5 minutes (not mandatory)
+		6: { 'Action' :2, 'P' : 100.0, 'Q' :0.0, 'maxSOC' :1, 'minSOC':0.0, 'Duration' : 0.0 * 60 }, #Charging
 		7: { 'Action' :0, 'P' :   0.0, 'Q' :0, 'maxSOC' :100, 'minSOC':0, 'Duration' : 5 }, # Pause for 5 minutes (not mandatory)
 		# #---Discharging/Charging with 100% nom. Power	(2of3)---
 		# 8: { 'Action' :1, 'P' : 100.0, 'Q' :0, 'maxSOC' :  0, 'minSOC':0, 'Duration' : 0 * 60 },	#Discharging
